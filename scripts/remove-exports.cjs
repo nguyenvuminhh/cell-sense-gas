1d86e9a24eee9728460d81d4c32ced6e6d5e7de1
nguyenvuminhh <vuminh25625@gmail.com>
2025-12-05 14:51:23 +0200

    fix file name

--- scripts/remove-exports.cjs
+++ scripts/remove-exports.cjs
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const targetFile = path.join(__dirname, '../dist/code.js'); // Change path if needed
+const targetFile = path.join(__dirname, '../dist/Code.js');
 const content = fs.readFileSync(targetFile, 'utf8');
 
 const stripped = content.replace(/export\s*{\s*[^}]*\s*};?\s*$/gm, '');
